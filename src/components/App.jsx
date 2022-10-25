76cbb7c105c048fc9b12d13abb9967a160e1166c
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2022-10-25 23:04:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <Container>
-        <h1>jhbjubkkyy</h1>
-      </Container>
+      <>
+        <h1>Hello</h1>
+      </>
     );
   }
 }
